c161ca8ac8cd3cf962ac61356a4295e0cc13ddb5
James MacCarthy <james.maccarthy@gmail.com>
2020-08-19 17:20:54 -0700

    test changes

--- app.py
+++ app.py
@@ -16,21 +16,22 @@ def index():
     category_tag = 'div.tease-card__picture > h2 > span'
     teaser_tag = None  # NBC Today - Good News does not have
 
-    page_content = funky.get_page_content(url, content_tag)
+    # page_content = funky.get_page_content(url, content_tag)
+    #
+    # today_good_articles = funky.pull_articles(
+    #     page_content,
+    #     article_tag,
+    #     title_tag,
+    #     category_tag,
+    #     teaser_tag
+    # )
+    #
+    # today_good_articles
+    #
+    # df = pd.DataFrame.from_dict(today_good_articles)
+    # positive_articles = df[df['positivity'] > 0.0]
+    # top5 = positive_articles[:5].to_dict(orient='records')
+    # sorted_top5 = sorted(top5, key=lambda x: x['positivity'], reverse=True)
 
-    today_good_articles = funky.pull_articles(
-        page_content,
-        article_tag,
-        title_tag,
-        category_tag,
-        teaser_tag
-    )
-
-    today_good_articles
-
-    df = pd.DataFrame.from_dict(today_good_articles)
-    positive_articles = df[df['positivity'] > 0.0]
-    top5 = positive_articles[:5].to_dict(orient='records')
-    sorted_top5 = sorted(top5, key=lambda x: x['positivity'], reverse=True)
-
-    return render_template('articles/index.html', articles=sorted_top5)
+    # return render_template('articles/index.html', articles=sorted_top5)
+    return render_template('base.html')
